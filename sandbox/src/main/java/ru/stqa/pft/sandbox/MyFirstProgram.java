1fe988a030d110553c916276d3418d969d6744f9
zacharewicz <polecenaksiezyc@gmail.com>
2017-03-19 17:58:20 +0100

    Revert ""Hello, Wspaniałe Pandy!""
    This reverts commit c14815208a819a534fb827544b6857160a27948a.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,7 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        String somebody = "Wspaniałe Pandy";
+        String somebody = "Pandy";
         System.out.println("Hello, " + somebody + " :-)");
 
         double l = 8.0;
