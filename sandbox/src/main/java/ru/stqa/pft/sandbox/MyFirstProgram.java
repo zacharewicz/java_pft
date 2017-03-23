ae13b4a52c8fb90c3befa0ee28c0b69a59a7d3d9
zacharewicz <polecenaksiezyc@gmail.com>
2017-03-24 00:50:49 +0100

    lesson 1, homework 2,  tasks 1-3/4

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,20 +2,25 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-  public static void main(String[] args) {
+  public static void main (String[] args) {
+    System.out.println("Hello, Alexei!");
 
+    Point p1 = new Point();
+    Point p2 = new Point();
 
-    Square s = new Square(5);
-    System.out.println("Powierzchnia kwadratu o boku " + s.l + " = " + s.area());
+    p1.x = 5.5;
+    p2.x = 3.3;
+    p1.y = 1.1;
+    p2.y = 8.8;
 
-    Rectangle r = new Rectangle(4,6);
-    System.out.println("Powierzchnia prostokąta o bokach " + r.a + " i " + r.b + " = " + r.area());
+    System.out.println("Point 1 has coodrinate (" + p1.x + ", " + p1.y + ")");
+    System.out.println("Point 2 has coodrinate (" + p2.x + ", " + p2.y + ")");
+    System.out.println("Distance between points is " + distance(p1,p2));
   }
 
-
-  public static double area(Rectangle r)
-  { return r.a * r.b; }
+  public static double distance(Point p1, Point p2){
+    return  Math.sqrt((p2.x - p1.x)*(p2.x - p1.x)+(p2.y - p1.y)*(p2.y - p1.y));
+  }
 }
-
 
 
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,11 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by zacharewicz on 2017-03-24.
+ */
+public class Point {
+  public double x;
+  public double y;
+}
+
+
