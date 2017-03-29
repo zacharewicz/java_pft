393a602dfb03c3cce65c686157e0477d195a5ec0
zacharewicz <polecenaksiezyc@gmail.com>
2017-03-30 01:38:16 +0200

    Lesson 2, end of movie 3 - funkcja Assert.assertEquals()

--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -1,17 +1,16 @@
 package ru.stqa.pft.sandbox;
-
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
  * Created by zacharewicz on 2017-03-29.
  */
 
-
 public class SquareTests {
 
   @Test
   public void testArea() {
     Square s = new Square(5);
-    assert s.area() == 25;
+    Assert.assertEquals(s.area(), 25.0);
   }
 }
