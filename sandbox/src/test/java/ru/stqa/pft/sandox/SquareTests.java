722d15aa1ed5689582c101141b8fa81c37839e64
zacharewicz <polecenaksiezyc@gmail.com>
2017-03-29 23:58:46 +0200

    Lesson 2, movie 3, 4min3sec - I cannot compile my first test :-(
    Information:java: Errors occurred while compiling module 'sandbox_test'
    Information:javac 1.8.0_121 was used to compile java sources
    Information:2017-03-29 23:48 - Compilation completed with 2 errors and 0
    warnings in 2s 289ms
    
    C:\Devel\java_pft\sandbox\src\test\java\ru\stqa\pft\sandox\SquareTests.java
    Error:(13, 5) java: cannot find symbol
    symbol:   class Square
    location: class ru.stqa.pft.sandox.SquareTests
    Error:(13, 20) java: cannot find symbol
    symbol:   class Square
    location: class ru.stqa.pft.sandox.SquareTests

---
+++ sandbox/src/test/java/ru/stqa/pft/sandox/SquareTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.sandox;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by zacharewicz on 2017-03-29.
+ */
+
+public class SquareTests {
+
+  @Test
+  public void testArea() {
+    Square s = new Square(5);
+    assert s.area() == 25;
+  }
+}
