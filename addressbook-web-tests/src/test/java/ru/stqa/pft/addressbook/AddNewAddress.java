726d635987b94b8b53fb2c372354827afcb5bcb8
zacharewicz <polecenaksiezyc@gmail.com>
2017-04-05 23:54:38 +0200

    Lesson 2, Homework 4 - NewAddressBookForClient)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddNewAddress.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddNewAddress.java
@@ -4,18 +4,10 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
-
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
 
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-
-import static org.openqa.selenium.OutputType.*;
 
 public class AddNewAddress {
   FirefoxDriver wd;
@@ -25,16 +17,16 @@ public class AddNewAddress {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     gotoMainPage();
-    login();
+    login("admin", "secret");
   }
 
-  private void login() {
+  private void login(String username, String password) {
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).click();
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
   }
 
@@ -44,8 +36,7 @@ public class AddNewAddress {
 
   @Test
   public void testAddNewAddress() {
-
-    createNewAddressBookForClient();
+    createNewAddressBookForClient("Anna", "Kowalska", "+48888888888", "anna.kowalska@gmail.com");
     submitNewAddressBookForClient();
     backToHomePage();
 
@@ -59,20 +50,20 @@ public class AddNewAddress {
     wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
   }
 
-  private void createNewAddressBookForClient() {
+  private void createNewAddressBookForClient(String ClientName, String ClientSurname, String ClientPhone, String ClientMail) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Anna");
+    wd.findElement(By.name("firstname")).sendKeys(ClientName);
     wd.findElement(By.name("middlename")).click();
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("Kowalska");
+    wd.findElement(By.name("lastname")).sendKeys(ClientSurname);
     wd.findElement(By.name("mobile")).click();
     wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys("+48888888888");
+    wd.findElement(By.name("mobile")).sendKeys(ClientPhone);
     wd.findElement(By.name("email")).click();
     wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys("anna.kowalska@gmail.com");
+    wd.findElement(By.name("email")).sendKeys(ClientMail);
   }
 
   @AfterMethod
