439cdf765b3f4734f27f43f92ba1737b714919f9
zacharewicz <polecenaksiezyc@gmail.com>
2017-04-05 23:34:57 +0200

    Lesson 2, Homework 4 - test: AddNewAddress (create parameters)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddNewAddress.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddNewAddress.java
@@ -4,18 +4,10 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
-
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
 
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-
-import static org.openqa.selenium.OutputType.*;
 
 public class AddNewAddress {
   FirefoxDriver wd;
@@ -25,16 +17,16 @@ public class AddNewAddress {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     gotoNewAddressPage();
-    login();
+    login("admin", "secret");
   }
 
-  private void login() {
+  private void login(String name, String password) {
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(name);
     wd.findElement(By.name("pass")).click();
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
   }
 
@@ -45,10 +37,10 @@ public class AddNewAddress {
   @Test
   public void testAddNewAddress() {
 
-    createName();
-    createSurname();
-    createPhone();
-    createMail();
+    createName("Anna");
+    createSurname("Kowalska");
+    createPhone("+48888888888");
+    createMail("anna.kowalska@gmail.com");
     submitNewAddress();
     gotoHomePage();
 
@@ -62,29 +54,29 @@ public class AddNewAddress {
     wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
   }
 
-  private void createMail() {
+  private void createMail(String email) {
     wd.findElement(By.name("email")).click();
     wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys("anna.kowalska@gmail.com");
+    wd.findElement(By.name("email")).sendKeys(email);
   }
 
-  private void createPhone() {
+  private void createPhone(String phone) {
     wd.findElement(By.name("mobile")).click();
     wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys("+48888888888");
+    wd.findElement(By.name("mobile")).sendKeys(phone);
   }
 
-  private void createSurname() {
+  private void createSurname(String surname) {
     wd.findElement(By.name("middlename")).click();
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("Kowalska");
+    wd.findElement(By.name("lastname")).sendKeys(surname);
   }
 
-  private void createName() {
+  private void createName(String name) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Anna");
+    wd.findElement(By.name("firstname")).sendKeys(name);
   }
 
   @AfterMethod
